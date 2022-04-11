70e7e7671f8ad301b58e51a26ab7c2cc0ebda77c
vladshkrebko <vladshkrebko@gmail.com>
2022-04-11 22:08:14 +0300

    Changes Tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int Num1 = int.Parse(Console.ReadLine());
+int Num2 = int.Parse(Console.ReadLine());
+if(Num2 == Num1*Num1)
+{
+Console.WriteLine("Num2 равно Num1^2");
+}
+else{
+    Console.WriteLine("Num2 не равно Num1^2");
+}
